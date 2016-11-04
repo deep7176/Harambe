aa2b27af2dfa97895d466007b194477c572e8d60
Deep <deep7176@gmail.com>
2016-11-04 19:25:16 -0400

    Initial commit

---
+++ app/src/main/java/com/harambe/app/harambe/AppBase.java
@@ -0,0 +1,27 @@
+package com.harambe.app.harambe;
+
+import android.app.Application;
+
+/**
+ * Created by deep on 8/24/16.
+ * the application singleton class
+ */
+public class AppBase extends Application {
+
+    //TODO change to false when in production
+    public static final boolean DEBUG = true;
+
+    public static AppBase instance;
+
+    public static boolean isDebug() {
+        return DEBUG;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppBase.instance = this;
+
+    }
+
+}
---
+++ app/src/main/java/com/harambe/app/harambe/GorillaActivity.java
@@ -0,0 +1,13 @@
+package com.harambe.app.harambe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GorillaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gorrilla);
+    }
+}
